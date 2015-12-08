119955957518d5415f08f5085dd44ce3d3f950de
zathyra <franziska.kissel@live.de>
2015-12-08 19:28:20 +0100

    jqueryUI Dialog hinzugefügt

--- LogIn.js
+++ LogIn.js
@@ -1,21 +1,58 @@
 $(document).ready(function(){
-    $("#submit").click(function(){
-        var username = $("#username").val();
-        var password = $("#password").val();
+    $.fn.inputrot = function(){
+        $(this).css("border","2px solid red");
+        $(this).css("background","#FFD9D9");
+        $(this).css("box-shadow","none");
+    };
 
-        if(username == "Username" && password == "Password"){
-            alert("Log In successful");
+    $.fn.checktext = function(){
+        var text = $(this).val();
+        //mind. 4 Zeichen, nur Buchstaben möglich aber nur Zahlen nicht, keine Sonderzeichen
+        var regex = new RegExp(/^([a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*){4,}$/);
+        return regex.test(text);
+    };
+
+    $.fn.alertinvalid = function(){
+        $("#username").inputrot();
+        $("#password").inputrot();
+        $("#alertpopup").dialog("open");
+    };
+
+    $("#alertpopup").dialog({
+        width: 400,
+        height: 150,
+        modal: true,
+        autoOpen: false,
+        draggable: false,
+        resizable: false,
+        buttons: {
+            Okay: function() {
+                $( this ).dialog( "close" );
+            }
         }
-        else{
-            $(".logininput").css("border","2px solid red");
-            $(".logininput").css("background","#FFD9D9");
-            $(".logininput").css("box-shadow","none");
-            alert("Invalid Username or Password.");
-        }
-
-
-
     });
 
-     //farbliche Hinterlegung bei aktivem Eingabefeld
+
+    $("#submit").click(function(){
+        if ($("#password").checktext() == true && $("username").checktext() == true) {
+            var username = $("#username").val();
+            var password = $("#password").val();
+
+            //hier später Ergebnis von Datenbankabfrage und direkte Weiterleitung auf die Seite (ohne Alert)
+            if (username == "Username" && password == "Password") {
+                alert("Log In successful");
+            }
+            else{
+                $("#username").inputrot();
+                $("#password").inputrot();
+                $("#alertpopup").dialog("open");
+            }
+        }
+        else{
+            $("#username").inputrot();
+            $("#password").inputrot();
+            $("#alertpopup").dialog("open");
+        }
+
+    });
 });
